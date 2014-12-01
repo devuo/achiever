4501b1c4139bc080c13f3c643e3e64ed1daace83
Diogo Correia <diogocorreia@devuo.net>
2014-12-01 18:17:35 +0000

    Show a different message if the user is seeing is own achievements.

--- theme/achiever-achievements.tpl.php
+++ theme/achiever-achievements.tpl.php
@@ -12,13 +12,21 @@
  *  The rendered achievements list.
  * @var object $account
  *  The user account to whom the achievement belongs to.
+ * @var bool $is_current_user
+ *  TRUE if the achievement belong to currently authenticated user.
  */
 
 ?>
 
 <section class="<?php print $classes ?>">
   <h1 class="achiever-achievements-title"><?php print t('Achievements'); ?></h1>
-  <p class="achiever-achievements-description"><?php print t("Here's the list of achievements."); ?></p>
+  <p class="achiever-achievements-description">
+    <?php if ($is_current_user): ?>
+      <?php print t("My achievements"); ?>
+    <?php else: ?>
+      <?php print t("The list of achievements"); ?>
+    <?php endif; ?>
+  </p>
 
   <div class="achiever-achievements-list">
     <?php foreach ($rendered_achievements as $rendered_achievement): ?>
